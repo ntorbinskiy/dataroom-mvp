a18bc1ae8579566d17ef654c524c56654c791c5d
ntorbinskiy <n.torbinskiy@gmail.com>
2026-07-17 15:11:17 +0300

    feat: add design tokens, fonts and shadcn ui kit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import '@fontsource/source-serif-4/400.css'
+import '@fontsource/source-serif-4/600.css'
+import '@fontsource/ibm-plex-sans/400.css'
+import '@fontsource/ibm-plex-sans/500.css'
+import '@fontsource/ibm-plex-sans/600.css'
+import '@fontsource/ibm-plex-mono/400.css'
+import '@fontsource/ibm-plex-mono/500.css'
 import './index.css'
 
 const rootElement = document.getElementById('root')
